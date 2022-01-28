288c3dfef29943a5ae2f4051540e225e7ad7012b
Juliana Ruiz <jruiz9312@gmail.com>
2022-01-28 15:01:10 -0800

    Initial Commit

---
+++ PlayOnV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayOnV1
+//
+//  Created by Juliana Ruiz  on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayOnV1/PlayOnV1App.swift
@@ -0,0 +1,17 @@
+//
+//  PlayOnV1App.swift
+//  PlayOnV1
+//
+//  Created by Juliana Ruiz  on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct PlayOnV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
