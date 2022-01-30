6a6fb33d6f670d2cf47524b1c3642a57a6c2a96a
Juliana Ruiz <jruiz9312@gmail.com>
2022-01-29 22:41:51 -0800

    playerPost Card created w/ image using hard coded data

--- PlayOnV1/Views/Components/PostCard.swift
+++ PlayOnV1/Views/Components/PostCard.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct PostCard: View {
+    var playerPost: PlayerPost
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            AsyncImage(url: URL(string: playerPost.image)) {image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .overlay(alignment: .bottom){
+                        Text(playerPost.title)
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .shadow(color: .black, radius: 3, x: 0, y: 0)
+                            .frame(maxWidth:136)
+                            .padding()
+                            
+                    }
+
+            } placeholder: {
+                Image(systemName: "image")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .foregroundColor(.white.opacity(0.7))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .overlay(alignment: .bottom){
+                        Text(playerPost.title)
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .shadow(color: .black, radius: 3, x: 0, y: 0)
+                            .frame(maxWidth:136)
+                            .padding()
+                            
+        }
+            
+        }
+        
+            .frame(width: 160, height: 217, alignment: .top)
+            .background(LinearGradient(gradient:Gradient(colors:[Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            .shadow(color: .black.opacity(0.3), radius: 15, x: 0, y: 10)
+        }
     }
 }
 
 struct PostCard_Previews: PreviewProvider {
     static var previews: some View {
-        PostCard()
+        PostCard(playerPost: PlayerPost.all[0])
     }
 }
+
